5317580af64b985c6e9efff65ac51972f0f9ce1d
Grama Gergő Zétény <b3grager@vasvari.org>
2025-11-10 14:13:52 +0100

    feladatmegint

---
+++ test gyakorlas.py
@@ -0,0 +1,21 @@
+import unittest
+import math
+
+szam1=int(input("szam1"))
+szam2=int(input("szam2"))
+
+def abs_max(a:int, b:int) -> int:
+    if(abs(a)>abs(b)):
+        return(abs(a))
+    if(abs(a)<abs(b)):
+        return(abs(b))
+    
+        
+print(abs_max(szam1,szam2))
+
+class Testek(unittest.Testcase):
+    def test_nagyobb_nulla(self):
+        nagyobb=int(abs_max(szam1,szam2))
+        self.assertGreater(nagyobb,0)
+
+unittest.main()

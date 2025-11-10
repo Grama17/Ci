08d60976aab7c3b5e01a53a9ff267979711027c9
Grama Gergő Zétény <b3grager@vasvari.org>
2025-11-10 14:23:46 +0100

    ujramegint

--- test gyakorlas.py
+++ test gyakorlas.py
@@ -22,6 +22,6 @@ class Testek(unittest.TestCase):
 class Testek(unittest.TestCase):
     def test_nagyobb_nulla(self):
         nagyobb=int(abs_max(szam1,szam2))
-        self.assertGreater(nagyobb,0)
+        self.assertGreater(nagyobb,99)
 
 unittest.main()
